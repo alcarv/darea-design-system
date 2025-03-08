2d86b87b6edf01dcc02fdd10e7235bf6b7dafd70
alefe <alefecarvalho@hotmail.com>
2025-03-08 15:28:31 -0300

    Add word site on See Example

--- src/App.tsx
+++ src/App.tsx
@@ -971,7 +971,7 @@ function App() {
                       rel="noopener noreferrer"
                       className="mb-4 inline-block border border-orange-500 bg-transparent text-white text-xs py-1 px-3 rounded-md transition-colors duration-300 flex items-center"
                     >
-                      See Example of {pkg.name}
+                      See Example of {pkg.name} Site
                       <Monitor className="ml-auto h-4 w-4 text-white" />
                     </a>
                     <div className="mb-4 flex items-center justify-between">
